fce98c5d012ad7ca01f3b7521a832c1298d55088
Khadziev <82869463+Khadziev@users.noreply.github.com>
2022-09-21 18:17:58 +0300

    адаптивнасть к разным устроиствам

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -9,7 +9,7 @@ function Search(props) {
       <span>Пользователь не найден!</span>
 
       <div className="userChat">
-        <img src="" alt="" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDxl2Nf4fQtL3CxjKLKmZ4DfCKs-qPKONdXdOll4uSwg&s" alt="" />
         <div className="userChatInfo">
           <span>Хаз</span>
         </div>
