71eef0955dc71e0717e50b0583eec80a1cb15126
Khadziev <82869463+Khadziev@users.noreply.github.com>
2022-09-22 18:38:50 +0300

    поиск пользователей и изменения внешнего вида страницы

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-function Message(props) {
-  return (
-    <div>
-      <div className="messageInfo">
-        <img src="" alt="" />
-        <span>сейчас</span>
-      </div>
-      <div className="messageContent">
-        <p>Текст</p>
-      </div>
-    </div>
-  );
-}
+const Message = ({ message }) => {
+    const { currentUser } = useContext(AuthContext);
+    const { data } = useContext(ChatContext);
+
+    const ref = useRef();
+
+    useEffect(() => {
+        ref.current?.scrollIntoView({ behavior: "smooth" });
+    }, [message]);
+
+    return (
+        <div
+            ref={ref}
+            className={`message ${message.senderId === currentUser.uid && "owner"}`}
+        >
+            <div className="messageInfo">
+                <img
+                    src={
+                        message.senderId === currentUser.uid
+                            ? currentUser.photoURL
+                            : data.user.photoURL
+                    }
+                    alt=""
+                />
+                <span>сейчас</span>
+            </div>
+            <div className="messageContent">
+                <p>{message.text}</p>
+                {message.img && <img src={message.img} alt="" />}
+            </div>
+        </div>
+    );
+};
 
 export default Message;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 import Search from "./Search";
 import Chats from "./Chats";
 
-function Sidebar(props) {
+function Sidebar() {
     return (
         <div className='sidebar'>
             <Navbar/>
